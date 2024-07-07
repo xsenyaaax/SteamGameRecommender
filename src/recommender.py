ba91c4eae8bce31558412c71610cdefd94bfea36
senyaaa <pogodars@fit.cvut.cz>
2024-07-07 14:54:41 +0200

    Backend code added

---
+++ src/recommender.py
@@ -0,0 +1,60 @@
+import time
+
+from SteamGameRecommender.src.steam_handler import SteamHandler
+import pandas as pd
+import joblib
+import asyncio
+
+
+class Recommender:
+    def __init__(self):
+        self.steam_handler = SteamHandler()
+        self.current_recommender = None  # knn with cosine similarity
+        self.expanded_tags = pd.read_csv('data/expanded_tags.csv')
+        self.expanded_tags.index = self.expanded_tags['AppID']
+        self.knn_cosine = joblib.load('models/tags.pkl')
+
+    async def recommend(self, steam_id):
+        if self.current_recommender is None:
+            games = await self.steam_handler.get_recently_played_games(steam_id)
+            recommended_ids = set()
+            for game in games:
+                recommended_ids.update(self.knn_recommender(game['appid']))
+
+        tasks = []
+        for app_id in recommended_ids:
+            tasks.append(self.steam_handler.get_app_info(app_id))
+        start_time = time.time()
+        results = await asyncio.gather(*tasks)
+        end_time = time.time()
+        print(f'Took {end_time - start_time} seconds to get {len(results)} results')
+        return results
+
+
+
+    def knn_recommender(self, game_id, num_recommendations=5):
+        query_vector = self.expanded_tags.loc[game_id].to_numpy()[1:].reshape(1, -1)
+        distances, indices = self.knn_cosine.kneighbors(query_vector, n_neighbors=num_recommendations + 1)
+
+        # Exclude the input game itself
+        indices = indices[0][1:num_recommendations + 1]
+
+        # Map indices back to AppIDs
+        recommended_app_ids = self.expanded_tags.index[indices].tolist()
+
+        return set(recommended_app_ids)
+        # Return the top 5 most similar games along with their tags
+        # return self.expanded_tags.loc[recommended_app_ids]
+
+
+#async def main():
+#    recommender = Recommender()
+#    await recommender.recommend(76561198168015547)
+
+
+#asyncio.run(main())
+
+# expanded_tags = pd.read_csv('data/expanded_tags.csv')
+# expanded_tags.index = expanded_tags['AppID']
+# print(expanded_tags.columns)
+# print("Current data shape:", expanded_tags.shape)
---
+++ src/steam_handler.py
@@ -0,0 +1,89 @@
+import aiohttp
+import asyncio
+from SteamGameRecommender.src.conf import API_KEY
+import time
+
+
+class SteamHandler:
+    def __init__(self):
+        self.base_url = 'https://api.steampowered.com'
+
+    async def get_owned_games(self, steam_id, include_appinfo=True, include_played_free_games=True, format='json'):
+        endpoint = f"{self.base_url}/IPlayerService/GetOwnedGames/v0001/"
+        params = {
+            'key': API_KEY,
+            'steamid': steam_id,
+            'include_appinfo': str(include_appinfo).lower(),
+            'include_played_free_games': str(include_played_free_games).lower(),
+            'format': format
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(endpoint, params=params) as response:
+                response.raise_for_status()
+                return await response.json()
+
+    async def get_recently_played_games(self, steam_id, count=None, format='json'):
+        endpoint = f"{self.base_url}/IPlayerService/GetRecentlyPlayedGames/v0001/"
+        params = {
+            'key': API_KEY,
+            'steamid': steam_id,
+            'format': format
+        }
+        if count is not None:
+            params['count'] = count
+        async with aiohttp.ClientSession() as session:
+            async with session.get(endpoint, params=params) as response:
+                response.raise_for_status()
+                data = await response.json()
+                try:
+                    return data['response']['games']
+                except KeyError:
+                    return None
+
+    async def get_app_info(self, app_id, format='json'):
+        endpoint = f"http://store.steampowered.com/api/appdetails?appids={app_id}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(endpoint) as response:
+                response.raise_for_status()
+                return await response.json()
+
+
+async def test_speed():
+    steam_ids = ['76561198168015547']
+    steam_handler = SteamHandler()
+    tasks = []
+    for steam_id in steam_ids:
+        tasks.append(steam_handler.get_recently_played_games(steam_id))
+    start_time = time.time()
+    results = await asyncio.gather(*tasks)
+    end_time = time.time()
+    print(f'Took {end_time - start_time} seconds to get {len(results)} results')
+
+
+async def test_request():
+    steam_ids = ['76561198168015547']
+    steam_handler = SteamHandler()
+    tasks = []
+    for steam_id in steam_ids:
+        tasks.append(steam_handler.get_recently_played_games(steam_id))
+    results = await asyncio.gather(*tasks)
+    for result in results:
+        print(result)
+
+
+async def test_app_info():
+    app_ids = ['570']
+    steam_handler = SteamHandler()
+    tasks = []
+    for app_id in app_ids:
+        tasks.append(steam_handler.get_app_info(app_id))
+    start_time = time.time()
+    results = await asyncio.gather(*tasks)
+    end_time = time.time()
+    print(f'Took {end_time - start_time} seconds to get {len(results)} results')
+    # for result in results:
+    #    print(result)     
+
+# asyncio.run(test_speed())
+# asyncio.run(test_app_info())
+# asyncio.run(test_request())
